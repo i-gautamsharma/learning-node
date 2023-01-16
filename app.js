fef120f76a687d1c6e2f90d9b764a53a677f65c3
gautam <gautamsharma.best@gmail.com>
2023-01-16 22:48:14 +0530

    getting json data from post

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 const { ids } = require("./data.js");
-app.use(express.static('./Web'))
-app.use(urlencoded({extended:false}))
-app.get("/",(req, res) => {
+app.use(express.static("./Web"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to HomePage...</h1>");
 });
+
 app.post("/login", (req, res) => {
-  const { Name, Password } = req.body
+  const { Name, Password } = req.body;
   if (Name) {
-    res.status(200).send(`Welcome ${Name} Your Credentials are : ${Password}`)
+    res.status(200).send(`Welcome ${Name} Your Credentials are : ${Password}`);
   } else {
-    res.status(401).send("Please Provide name And Credentials")
+    res.status(401).send("Please Provide name And Credentials");
   }
 });
 
-
-
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ success: true, data: ids });
+});
 
 app.listen(3300, () => {
-  console.log("app is listening....");
+  console.log("app is listening on 3300....");
 });
