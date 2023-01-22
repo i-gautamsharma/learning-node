2b6b1c2e30cbc6346db006df6ea11bd9433f9b62
gautam <gautamsharma.best@gmail.com>
2023-01-22 23:28:45 +0530

    no changes

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use('/search',search)
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to HomePage...</h1>");
 });
-
-
 app.listen(3300, () => {
   console.log("app is listening on 3300....");
 });
